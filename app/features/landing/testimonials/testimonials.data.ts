e2883127d7b4f7b6946b37c95cc3dff5209a23de
knight-raj <kinghtraj09@gmail.com>
2026-08-15 20:43:55 +0530

    feat: test

--- app/features/landing/testimonials/testimonials.data.ts
+++ app/features/landing/testimonials/testimonials.data.ts
@@ -11,7 +11,7 @@ export const categories = [
 
 export const testimonials: TestimonialData[] = [
   {
-    audioSrc: "",
+    audioSrc: "/testimonials/audio/mizuho.mp3",
     imgSrc: "/placeholder.svg",
     alt: "Mizuho",
     quote:
@@ -34,37 +34,130 @@ export const testimonials: TestimonialData[] = [
       "The key to success was due to collaborative efforts and fast implementation of GrowthOps and our teams.",
     position: "Digital Analytics & Insights, Decision Management",
   },
+
+  // Insurance
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "GrowthOps is a trustworthy partner that I would recommend to any company.",
+    position: "Associate Director, Digitalization, Innovation and Enterprise Architecture",
+  },
+
+  // Telco
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "GrowthOps have shown to have the right capabilities to take our platform to the next level.",
+    position: "Chief Digital Officer",
+  },
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "GrowthOps can target a single creative concept with two perspectives.",
+    position: "Vice President, Brand & Marketing",
+  },
+
+  //Travel
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "GrowthOps has brought the right maturity in design, technology, and analytics capabilities to take our platform to the next level.",
+    position: "Group Chief Digital and Technology Officer",
+  },
+
+  //Superapp
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "GrowthOps have gone above and beyond.",
+    position: "Content Head",
+  },
+
+  //More
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "GrowthOps has shown that the team is capable of providing one of the highest standards in project management and strategy.",
+    position: "Head of Digital",
+  },
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "We are leveraging the agency’s digital marketing capabilities to showcase the depth of progress that RSPO Members are driving in the quest to make palm oil sustainable.",
+    position: "Deputy Director of Communications",
+  },
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "This new campaign [created by GrowthOps] aims to go beyond our product benefits and connect with Malaysians.",
+    position: "GM, Shell Mobility Malaysia",
+  },
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "GrowthOps has been a key partner to Crown’s digital growth in the Asia market.",
+    position: "Digital Marketing Manager",
+  },
+  {
+    audioSrc: "",
+    imgSrc: "/placeholder.svg",
+    alt: "company logo",
+    quote:
+      "We chose to partner with GrowthOps in implementing Salesforce as we want to make sure that we are able to maximize its features.",
+    position: "Chief Executive Officer",
+  },
+  
+    
+
 ];
 
 export const logos: LogoData[] = [
-  { src: "/placeholder.svg", alt: "DBS x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "RHB x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "AIA x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "UOB x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Mastercard x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Emirates x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "CIMB x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Telekom Malaysia x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "U Mobile x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Firefly Airlines x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Malaysia Airlines x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Taylor's Universityx GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Grab x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Esplanade x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "HLB x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Unifi x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Mizuho x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Singlife x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Manulife x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "FWD x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Proton x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Amway x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Maxis x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Bega x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Farmer's Union x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Unicharm x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Shell x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "RSPO x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "Upvio x GrowthOps Asia" },
-  { src: "/placeholder.svg", alt: "SPH x GrowthOps Asia" },
+  { src: "/testimonials/logo/dbs.webp", alt: "DBS x GrowthOps Asia" },
+  { src: "/testimonials/logo/rhb.webp", alt: "RHB x GrowthOps Asia" },
+  { src: "/testimonials/logo/aia-growthops-asia.webp", alt: "AIA x GrowthOps Asia" },
+  { src: "/testimonials/logo/uob.webp", alt: "UOB x GrowthOps Asia" },
+  { src: "/testimonials/logo/mastercard-growthops-asia.webp", alt: "Mastercard x GrowthOps Asia" },
+  { src: "/testimonials/logo/emirates.webp", alt: "Emirates x GrowthOps Asia" },
+  { src: "/testimonials/logo/cimb.webp", alt: "CIMB x GrowthOps Asia" },
+  { src: "/testimonials/logo/telekom-malaysia-asia.webp", alt: "Telekom Malaysia x GrowthOps Asia" },
+  { src: "/testimonials/logo/u-mobile.webp", alt: "U Mobile x GrowthOps Asia" },
+  { src: "/testimonials/logo/firefly-airlines-asia.webp", alt: "Firefly Airlines x GrowthOps Asia" },
+  { src: "/testimonials/logo/malaysia-airlines.webp", alt: "Malaysia Airlines x GrowthOps Asia" },
+  { src: "/testimonials/logo/taylors-university.webp", alt: "Taylor's Universityx GrowthOps Asia" },
+  { src: "/testimonials/logo/grab.webp", alt: "Grab x GrowthOps Asia" },
+  { src: "/testimonials/logo/sph-growthops-asia.webp", alt: "Esplanade x GrowthOps Asia" },
+  { src: "/testimonials/logo/hlb.webp", alt: "HLB x GrowthOps Asia" },
+  { src: "/testimonials/logo/unifi-growthops-asia.webp", alt: "Unifi x GrowthOps Asia" },
+  { src: "/testimonials/logo/mizuho.webp", alt: "Mizuho x GrowthOps Asia" },
+  { src: "/testimonials/logo/singlife.webp", alt: "Singlife x GrowthOps Asia" },
+  { src: "/testimonials/logo/manulife.webp", alt: "Manulife x GrowthOps Asia" },
+  { src: "/testimonials/logo/fwd.webp", alt: "FWD x GrowthOps Asia" },
+  { src: "/testimonials/logo/proton.webp", alt: "Proton x GrowthOps Asia" },
+  { src: "/testimonials/logo/amway-growthops-asia.webp", alt: "Amway x GrowthOps Asia" },
+  { src: "/testimonials/logo/maxis-growthops-asia.webp", alt: "Maxis x GrowthOps Asia" },
+  { src: "/testimonials/logo/bega-growthops-asia.webp", alt: "Bega x GrowthOps Asia" },
+  { src: "/testimonials/logo/farmers-union-asia.webp", alt: "Farmer's Union x GrowthOps Asia" },
+  { src: "/testimonials/logo/unicharm-growthops-asia.webp", alt: "Unicharm x GrowthOps Asia" },
+  { src: "/testimonials/logo/shell-growthops-asia.webp", alt: "Shell x GrowthOps Asia" },
+  { src: "/testimonials/logo/rspo-growthops-asia.webp", alt: "RSPO x GrowthOps Asia" },
+  { src: "/testimonials/logo/upvio-growthops-asia.webp", alt: "Upvio x GrowthOps Asia" },
+  { src: "/testimonials/logo/sph-growthops-asia.webp", alt: "SPH x GrowthOps Asia" },
 ];
